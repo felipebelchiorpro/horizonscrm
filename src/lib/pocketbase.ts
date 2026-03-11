4a05aa4494a3f5d6bb04098d7a2b767d768cb03b
Felipe Belchior <contatofelipebelchior@gmail.com>
2026-03-11 15:48:04 -0300

    refactor: use environment variable for PocketBase URL

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -2,7 +2,8 @@ import PocketBase from 'pocketbase';
 
 // Connect to local or remote PocketBase instance
 // Connect to remote PocketBase instance
-export const pb = new PocketBase('https://backventure.venturexp.pro');
+const PB_URL = import.meta.env.VITE_POCKETBASE_URL || 'https://backventure.venturexp.pro';
+export const pb = new PocketBase(PB_URL);
 
 // Optional: Automatically disable auto cancellation so requests don't abort unnecessarily
 pb.autoCancellation(false);
